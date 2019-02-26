4ba40307caebb98ab68483c142b4f7c267e1f9af
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-02-26 15:44:14 -0600

    First commit

---
+++ README.rst
@@ -0,0 +1,7 @@
+========
+Bacanora
+========
+
+
+Implements accelerated, tenacious versions of Agave files **upload**
+and **download**, as well as a **grant** action for assigning permissions.
---
+++ setup.py
@@ -0,0 +1,90 @@
+from setuptools import setup, find_packages
+
+import copy
+import sys
+from pprint import pprint
+
+def get_version():
+    """
+    returns version.
+    """
+    return '0.0.1'
+
+def get_requirements(remove_links=True):
+    """
+    lists the requirements to install.
+    """
+    requirements = []
+    try:
+        with open('requirements.txt') as f:
+            requirements = f.read().splitlines()
+    except Exception as ex:
+        raise OSError('Failed to read in requirements.txt file', ex)
+    new_requirements = copy.copy(requirements)
+    if remove_links:
+        for requirement in requirements:
+            # git repository url.
+            if requirement.startswith("git+"):
+                new_requirements.remove(requirement)
+            # subversion repository url.
+            if requirement.startswith("svn+"):
+                new_requirements.remove(requirement)
+            # mercurial repository url.
+            if requirement.startswith("hg+"):
+                new_requirements.remove(requirement)
+    return new_requirements
+
+def get_links():
+    """
+    gets URL Dependency links.
+    """
+    links_list = get_requirements(remove_links=False)
+    for link in links_list:
+        keep_link = False
+        already_removed = False
+        # git repository url.
+        if not link.startswith("git+"):
+            if not link.startswith("svn+"):
+                if not link.startswith("hg+"):
+                    links_list.remove(link)
+                    already_removed = True
+                else:
+                    keep_link = True
+                if not keep_link and not already_removed:
+                    links_list.remove(link)
+                    already_removed = True
+            else:
+                keep_link = True
+            if not keep_link and not already_removed:
+                links_list.remove(link)
+    return links_list
+
+if not get_version():
+    raise RuntimeError('Version is not set')
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bacanora",
+    version=get_version(),
+    author="Matthew Vaughn",
+    author_email="opensource@tacc.cloud",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/SD2E/bacanora",
+    install_requires=get_requirements(),
+    dependency_links=get_links(),
+    packages=find_packages(),
+    license="BSD",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+    ]
+)
