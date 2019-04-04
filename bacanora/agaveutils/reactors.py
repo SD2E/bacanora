fed536e6d09d41beb626d952b602d9a566d1c6e6
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-04-04 13:30:56 -0500

    Added bacanora.agaveutils.await_execution to enablke sychronous, blocking messages. Extended message_reactor to honor its 'sync' keyword argument

--- bacanora/agaveutils/reactors.py
+++ bacanora/agaveutils/reactors.py
@@ -4,9 +4,12 @@ Functions for working with TACC Reactors
 import re
 import os
 import time
-from agavepy.agave import Agave
+from agavepy.agave import Agave, AgaveError
 from attrdict import AttrDict
-
+from tenacity import retry, retry_if_exception_type
+from tenacity import stop_after_delay
+from tenacity import wait_exponential
+from pprint import pprint
 
 MAX_ELAPSED = 300
 MAX_RETRIES = 5
@@ -16,9 +19,17 @@ MAX_RETRIES = 5
 # TODO: Support binary FIFO?
 
 
-def message_reactor(agaveClient, actorId, message,
-                    environment={}, ignoreErrors=True,
-                    sync=False, senderTags=True,
+class ExecutionNotComplete(AgaveError):
+    pass
+
+
+def message_reactor(agaveClient,
+                    actorId,
+                    message,
+                    environment={},
+                    ignoreErrors=True,
+                    sync=False,
+                    senderTags=True,
                     timeOut=MAX_ELAPSED):
     """
     Send a message to an Abaco actor by its ID
@@ -26,11 +37,13 @@ def message_reactor(agaveClient, actorId, message,
     Returns execution ID. If ignoreErrors is True, this is fire-and-forget.
     Otherwise, failures raise an Exception to handled by the caller.
     """
-    SPECIAL_VARS = {'_abaco_actor_id': 'x_src_actor_id',
-                    '_abaco_execution_id': 'x_src_execution_id',
-                    'JOB_ID': 'x_src_job_id',
-                    'EVENT': 'x_src_job_event',
-                    '_event_uuid': 'x_external_event_id'}
+    SPECIAL_VARS = {
+        '_abaco_actor_id': 'x_src_actor_id',
+        '_abaco_execution_id': 'x_src_execution_id',
+        'JOB_ID': 'x_src_job_id',
+        'EVENT': 'x_src_job_event',
+        '_event_uuid': 'x_external_event_id'
+    }
     pass_envs = {}
     if senderTags is True:
         for env in list(SPECIAL_VARS.keys()):
@@ -39,9 +52,8 @@ def message_reactor(agaveClient, actorId, message,
 
     execution = {}
     try:
-        execution = agaveClient.actors.sendMessage(actorId=actorId,
-                                                   body={'message': message},
-                                                   environment=pass_envs)
+        execution = agaveClient.actors.sendMessage(
+            actorId=actorId, body={'message': message}, environment=pass_envs)
     except Exception as e:
         errorMessage = "Error messaging actor {}: {}".format(actorId, e)
         if ignoreErrors:
@@ -49,12 +61,37 @@ def message_reactor(agaveClient, actorId, message,
         else:
             raise Exception(errorMessage)
 
+    execId = execution.get('executionId', None)
+
+    if sync is False:
+        try:
+            return execId
+        except Exception as e:
+            errorMessage = " Message to {} failed: {}".format(actorId, e)
+            if ignoreErrors:
+                pass
+            else:
+                raise Exception(errorMessage)
+    else:
+        return await_execution(
+            agaveClient, actorId=actorId, executionId=execId)
+
+
+@retry(
+    retry=retry_if_exception_type(AgaveError),
+    reraise=True,
+    stop=stop_after_delay(60),
+    wait=wait_exponential(multiplier=1, max=16))
+def await_execution(agaveClient, actorId, executionId):
     try:
-        execId = execution.executionId
-        return execId
-    except Exception as e:
-        errorMessage = " Message to {} failed: {}".format(actorId, e)
-        if ignoreErrors:
-            pass
+        execution_resp = agaveClient.actors.getExecution(
+            actorId=actorId, executionId=executionId)
+        pprint(execution_resp)
+        status = execution_resp.get('status', 'UNKNOWN')
+        if status in ['COMPLETE', 'FAILED', 'ERROR']:
+            return True
         else:
-            raise Exception(errorMessage)
+            raise ExecutionNotComplete('{}:{}.status is {}'.format(
+                actorId, executionId, status))
+    except Exception:
+        raise
