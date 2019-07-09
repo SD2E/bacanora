094f55293f96ab84ea66c4828ac041f8853782dc
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-07-09 16:35:17 -0500

    Imported walk() and listdir() into bacanora.files namespace

--- bacanora/files.py
+++ bacanora/files.py
@@ -1,4 +1,4 @@
 """Assorted file-oriented operations
 """
 from .operations import (get, put, exists, isfile, isdir, copy, rename, move,
-                         delete)
+                         delete, walk, listdir)
